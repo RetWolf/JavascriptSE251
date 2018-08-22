d7168f4564f701d4d5ee754361b7a7bd4f07f203
RetWolf <mattconway55@gmail.com>
2018-08-22 14:35:22 -0400

    Finished functionality and styling of Week6 stuff

--- Week6/index.js
+++ Week6/index.js
@@ -26,7 +26,87 @@ const showData = async () => {
   }
 }
 
-function parseData(data) {
+const searchPerson = async () => {
+  const fname = document.getElementById('fname');
+  const lname = document.getElementById('lname');
+  const resultsDiv = document.getElementById('search_Results');
+  resultsDiv.innerHTML = '';
+  let data = await getData();
+  let resultPerson = {};
+  data.forEach(person => {
+    if ((`${person.name.first} ${person.name.last}`) === (`${fname.value} ${lname.value}`)) {
+      resultPerson = person;
+    }
+  });
+  if (resultPerson.name !== undefined) {
+    Object.entries(resultPerson).forEach(([key, val]) => {
+      let h3 = document.createElement('h3');
+      if (key === 'name') {
+        h3.innerHTML = `Name: ${val.first} ${val.last}`;
+      } else if (key === 'salary') {
+        h3.innerHTML = `Salary: $${Number(val).toFixed(2)}`;
+      } else {
+        h3.innerHTML= `Job Title: ${val}`;
+      }
+      resultsDiv.appendChild(h3);
+    });
+  } else {
+    let h3 = document.createElement('h3');
+    h3.innerHTML = 'No person found with that name.';
+    resultsDiv.appendChild(h3);
+  }
+}
+
+document.getElementById('search_Person').addEventListener('click', searchPerson);
+
+const listResults = async (e) => {
+  const listDiv = document.getElementById('list_Results');
+  listDiv.innerHTML = '';
+  let index = 0;
+  if (e.target.id === 'listDBAnalysts') { index = 0 }
+  else if (e.target.id === 'listWebDevs') { index = 1 }
+  else { index = 2 }
+
+  let data = await getData();
+  let jobs = ['Database Analyst', 'Web Developer', 'Software Developer'];
+  let titles = ['Name', 'Salary', 'Job Title'];
+
+  let table = document.createElement('table');
+  let thead = document.createElement('thead');
+  let tbody = document.createElement('tbody');
+  for (let i = 0; i < titles.length; i++) {
+    let th = document.createElement('th');
+    th.innerHTML = titles[i];
+    thead.appendChild(th);
+  }
+  table.appendChild(thead);
+  
+  for (person of data) {
+    if (person.jobTitle === jobs[index]) {
+      let tr = document.createElement('tr');
+      Object.entries(person).forEach(([key, val]) => {
+        let td = document.createElement('td');
+        if (key === 'name') {
+          td.innerHTML = `${val.first} ${val.last}`
+        } else if (key === 'salary') {
+          td.innerHTML = `$${Number(val).toFixed(2)}`
+        } else {
+          td.innerHTML = val
+        }
+        tr.appendChild(td);
+      });
+      tbody.appendChild(tr);
+    }
+  }
+  table.appendChild(tbody);
+  listDiv.appendChild(table);
+}
+
+document.getElementById('listDBAnalysts').addEventListener('click', listResults);
+document.getElementById('listWebDevs').addEventListener('click', listResults);
+document.getElementById('listSoftwareDevs').addEventListener('click', listResults);
+
+function parseData (data) {
   let numOfDBAnalysts = 0;
   let numOfWebDevs = 0;
   let numOfSoftwareDevs = 0;
@@ -67,4 +147,8 @@ function parseData(data) {
   return people;
 }
 
-showData();
+if (document.readyState === 'loading') {
+  document.addEventListener('DOMContentLoaded', showData)
+} else {
+  showData();
+}
